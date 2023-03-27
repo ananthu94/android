b7d96533ce2179f06efb3d7a90808036f901afb4
ananthu94 <ananthu94c@gmail.com>
2023-03-27 16:11:55 +0530

    1

---
+++ bin/a2.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = 'ananthu';
+  int age = 28;
+  double mark = 7.5;
+  String email = 'an@yahoo.com';
+
+  print('NAME : $name');
+  print('AGE  : $age');
+  print('MARK : $mark');
+  print('EMAIL: $email');
+}
